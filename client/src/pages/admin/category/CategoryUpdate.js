2e15fe74ee19b780f0722132cde10a84d00e7e0c
Antony Parker <antonyqparker@gmail.com>
2021-03-23 17:02:07 +0100

    Updating category

---
+++ client/src/pages/admin/category/CategoryUpdate.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import AdminNav from "../../../components/nav/AdminNav";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import {
+  getCategory,
+  updateCategory
+} from "../../../actions/category";
+
+// Create Category
+const CategoryUpdate = ({history, match}) => {
+  const { user } = useSelector((state) => ({ ...state }));
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  
+
+
+  //   Get Categories
+  useEffect(() => {
+    loadCategory()
+  }, []);
+
+  const loadCategory = () =>
+    getCategory(match.params.slug).then((c) => setName(c.data.name));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    updateCategory(match.params.slug, { name: name }, user.token)
+      .then((res) => {
+        console.log(res);
+        setLoading(false);
+        setName("");
+        toast.success(`"${res.data.name}" category has been updated`);
+        history.push('/admin/category')
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        if (err.response.status === 400) toast.error(err.response.data);
+      });
+  };
+
+//   const handleRemove = async(slug) => {
+//     if(window.confirm("Are you sure you would like to delete?")){
+//         setLoading(true)
+//         removeCategory(slug, user.token)
+//         .then(res => {
+//             setLoading(false)
+//             loadCategories()
+//             toast.error(`${res.data.msg}`)
+//         })
+//         .catch(err => {
+//             if (err.response.status === 400){
+//                 toast.error(err.response.data);
+//                 setLoading(false)
+//             } 
+//         })
+//     }
+//   }
+
+  const categoryForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className='form-group'>
+        <label>Name</label>
+        <input
+          type='text'
+          className='form-control'
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          autoFocus
+          required
+        />
+        <button className='btn btn-outline-primary mt-2'>Save</button>
+      </div>
+    </form>
+  );
+
+  return (
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-2'>
+          <AdminNav />
+        </div>
+        <div className='col'>
+          {loading ? (
+            <h4 className='text-danger'>Loading..</h4>
+          ) : (
+            <h4>update category</h4>
+          )}
+          {categoryForm()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryUpdate;
