dd3238d207aadbe7fd5362b3b15fbcab483452d2
Antony Parker <antonyqparker@gmail.com>
2021-03-29 22:23:48 +0200

    Added product update route and page

---
+++ client/src/pages/admin/product/ProductUpdate.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import AdminNav from "../../../components/nav/AdminNav";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { createProduct } from "../../../actions/product";
+import { getCategories } from "../../../actions/category";
+import ProductCreateForm from "../../../components/forms/ProductCreateForm";
+import FileUpload from "../../../components/forms/FileUpload";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const ProductUpdate = () => {
+  // Fetch user from state
+  const { user } = useSelector((state) => ({ ...state }));
+
+  return (
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-2'>
+          <AdminNav />
+        </div>
+        <div className='col-md-10'>
+          <h4>Product Update</h4> 
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductUpdate;
