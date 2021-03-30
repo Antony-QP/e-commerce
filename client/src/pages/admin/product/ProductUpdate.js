360b9f04000a79d0ad13374eb4d47706d7b5fd93
Antony Parker <antonyqparker@gmail.com>
2021-03-30 12:12:08 +0200

    Adding routes and displaying product in console

--- client/src/pages/admin/product/ProductUpdate.js
+++ client/src/pages/admin/product/ProductUpdate.js
@@ -2,15 +2,28 @@ import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
-import { createProduct } from "../../../actions/product";
+import { createProduct, getProduct } from "../../../actions/product";
 import { getCategories } from "../../../actions/category";
 import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 import FileUpload from "../../../components/forms/FileUpload";
 import { LoadingOutlined } from "@ant-design/icons";
 
-const ProductUpdate = () => {
+const ProductUpdate = ({ match }) => {
   // Fetch user from state
   const { user } = useSelector((state) => ({ ...state }));
+
+  const { slug } = match.params;
+
+  useEffect(() => {
+    loadProduct()
+  }, [])
+
+  const loadProduct = () => {
+    getProduct(slug)
+    .then(p => {
+      console.log(p)
+    })
+  }
 
   return (
     <div className='container-fluid'>
