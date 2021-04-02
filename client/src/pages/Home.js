43763de9812c84da6338032a959837a7746758e9
Antony Parker <antonyqparker@gmail.com>
2021-04-02 16:47:02 +0200

    Adding loading cards to hompage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { getProductsByCount } from "../actions/product";
 import ProductCard from "../components/cards/ProductCard";
-import Jumbotron from '../components/cards/Jumbotron'
+import Jumbotron from "../components/cards/Jumbotron";
+import LoadingCard from "../components/cards/LoadingCard";
 
 export const Home = () => {
   const [products, setProducts] = useState([]);
@@ -22,17 +23,21 @@ export const Home = () => {
   return (
     <>
       <div className='jumbotron text-danger h1 font-weight-bold text-center'>
-          <Jumbotron text={["New Arrivals", "Best Sellers", "Latest Products"]}/>
+        <Jumbotron text={["New Arrivals", "Best Sellers", "Latest Products"]} />
       </div>
 
       <div className='container'>
-        <div className='row'>
-          {products.map((product) => (
-            <div key={product._id} className='col-md-4'>
-              <ProductCard product={product} />
-            </div>
-          ))}
-        </div>
+        {loading ? (
+          <LoadingCard count={3}/>
+        ) : (
+          <div className='row'>
+            {products.map((product) => (
+              <div key={product._id} className='col-md-4'>
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
