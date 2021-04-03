dd78c0b4a7292ab27ecb187535cb25c1f227b4e3
Antony Parker <antonyqparker@gmail.com>
2021-04-03 10:48:15 +0200

    Fetching product count from the database

--- server/routes/product.js
+++ server/routes/product.js
@@ -6,10 +6,12 @@ const {authCheck, adminCheck} = require('../middlewares/auth')
 
 
 // Import Controllers
-const { create, listAll, remove, read, update, list } = require('../controllers/product')
+const { create, listAll, remove, read, update, list, productCount } = require('../controllers/product')
 
 // Routes
 router.post('/product', authCheck, adminCheck, create);
+// Pagination
+router.get('/products/total', productCount)
 router.get('/products/:count', listAll);
 router.delete('/product/:slug', authCheck, adminCheck, remove)
 router.put('/product/:slug', authCheck, adminCheck, update)
@@ -17,6 +19,7 @@ router.get('/product/:slug', read)
 
 // List recently added and best sellers
 router.post('/products', list)
+
 
 
 module.exports = router
