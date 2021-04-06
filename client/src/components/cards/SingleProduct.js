05e1c6e81e392c41b2d3248d878bc5cdc9c9986b
Antony Parker <antonyqparker@gmail.com>
2021-04-06 15:13:28 +0200

    Displaying default images for products without images

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -3,7 +3,8 @@ import { Card } from "antd";
 import { Link } from "react-router-dom";
 import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
+import Laptop from "../../images/default.png";
 
 const { Meta } = Card;
 
@@ -12,24 +13,34 @@ const SingleProduct = ({ product }) => {
   return (
     <>
       <div className='col-md-7'>
-      <Carousel showArrows={true} autoPlay infiniteLoop={true}>
-          {images && images.map((i) => <img src={i.url} key={i.public_id}/> )}
-      </Carousel>
+        {images && images.length ? (
+          <Carousel showArrows={true} autoPlay infiniteLoop={true}>
+            {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
+          </Carousel>
+        ) : (
+          <Card
+            cover={
+              <img
+                src={Laptop}
+                style={{ height: "450", objectFit: "cover" }}
+                className='mb-3'
+              />
+            }></Card>
+        )}
       </div>
 
       <div className='col-md-5'>
-        
-        <Card actions={[
-        <>
-          <ShoppingCartOutlined className='text-success' /> Add to cart
-        </>
-        ,
-        <Link to='/'>
-          <HeartOutlined className='text-info' /><br/>
-          Add To Wishlist
-        </Link>
-        ]}>
-            
+        <Card
+          actions={[
+            <>
+              <ShoppingCartOutlined className='text-success' /> Add to cart
+            </>,
+            <Link to='/'>
+              <HeartOutlined className='text-info' />
+              <br />
+              Add To Wishlist
+            </Link>,
+          ]}>
           <Meta title={title} description={description} />
         </Card>
       </div>
