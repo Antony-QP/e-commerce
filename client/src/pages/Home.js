a7febd6df3d65b30aa6f15688abb4779a85c99f5
Antony Parker <antonyqparker@gmail.com>
2021-04-08 15:03:02 +0200

    Displaying category tabs on the home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 import NewArrivals from "../components/home/NewArrivals"
 import BestSellers from "../components/home/BestSellers"
+import CategoryList from '../components/category/CategoryList'
 
 export const Home = () => {
 
@@ -16,6 +17,9 @@ export const Home = () => {
 
       <h4 className="text-center p-3 mt-5 mb-5 jumbotron">Best Sellers</h4>
       <BestSellers/>
+
+      <h4 className="text-center p-3 mt-5 mb-5 jumbotron">Categories</h4>
+      <CategoryList/>
     </>
   );
 };
