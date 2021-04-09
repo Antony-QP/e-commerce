20bf0b0cf5e1e30b15d94c45b575f34c9fb0e2ce
Antony Parker <antonyqparker@gmail.com>
2021-04-09 14:09:54 +0200

    Building search endpoint in routes and building controller

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -148,3 +148,23 @@ exports.listRelated = async(req, res) => {
 
   res.json(related)
 }
+
+// Search and Filters
+
+const handleQuery = async(req, res, query) => {
+  const products = await Product.find({$text: { search: query }})
+  .populate('category', '_id name')
+  populate('postedBy', '_id name')
+  .exec();
+
+  res.json(products)
+}
+
+exports.searchFilters = async(req, res) => {
+    const { query } = req.body
+
+    if(query){
+      console.log('query', query)
+      await handleQuery(req, res, query)
+    }
+} 
