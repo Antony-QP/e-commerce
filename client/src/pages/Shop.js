dd0e6dd578e5079842ef59f8149894f70e6310a3
Antony Parker <antonyqparker@gmail.com>
2021-04-13 15:24:03 +0200

    Fetching products by brand from fe

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -3,10 +3,14 @@ import { getProductsByCount } from "../actions/product";
 import { useSelector, useDispatch } from "react-redux";
 import ProductCard from "../components/cards/ProductCard";
 import { fetchProductsByFilter } from "../actions/product";
-import { Menu, Slider, Checkbox } from "antd";
-import { DollarOutlined, DownSquareOutlined, StarOutlined } from "@ant-design/icons";
+import { Menu, Slider, Checkbox, Radio } from "antd";
+import {
+  DollarOutlined,
+  DownSquareOutlined,
+  StarOutlined,
+} from "@ant-design/icons";
 import { getCategories } from "../actions/category";
-import Star from '../components/forms/Star'
+import Star from "../components/forms/Star";
 
 const { SubMenu, ItemGroup } = Menu;
 
@@ -18,6 +22,15 @@ export const Shop = () => {
   const [categories, setCategories] = useState([]);
   const [categoryIds, setCategoryIds] = useState([]);
   const [star, setStar] = useState("");
+  const [brand, setBrand] = useState("");
+  const [brands, setBrands] = useState([
+    "Apple",
+    "Samsung",
+    "Microsoft",
+    "ASUS",
+    "Toshiba",
+    "Lenovo",
+  ]);
 
   let dispatch = useDispatch();
   let { search } = useSelector((state) => ({ ...state }));
@@ -57,7 +70,6 @@ export const Shop = () => {
 
   // Load products based on product range
   useEffect(() => {
-    console.log("okay to request");
     fetchProducts({ price });
   }, [ok]);
 
@@ -69,7 +81,8 @@ export const Shop = () => {
     });
     setPrice(value);
     setCategoryIds([]);
-    setStar('')
+    setStar("");
+    setBrand("");
     setTimeout(() => {
       setOk(!ok);
     }, 300);
@@ -97,7 +110,8 @@ export const Shop = () => {
       payload: { text: "" },
     });
     setPrice([0, 0]);
-    setStar('');
+    setStar("");
+    setBrand("");
     let inTheState = [...categoryIds];
     let justChecked = e.target.value;
     let foundInTheState = inTheState.indexOf(justChecked);
@@ -117,33 +131,61 @@ export const Shop = () => {
 
   // Load products for star value
   const handleStarClick = (num) => {
-    console.log(num)
+    console.log(num);
     dispatch({
       type: "SEARCH_QUERY",
       payload: { text: "" },
     });
     setPrice([0, 0]);
     setCategoryIds([]);
-    setStar(num)
-    fetchProducts({ stars: num })
-  }
-  const showStars = () => (
-    <div className="pr-4 pl-4 pb-2">
-      <Star starClick={handleStarClick} numberOfStars={5}/>
-      <Star starClick={handleStarClick} numberOfStars={4}/>
-      <Star starClick={handleStarClick} numberOfStars={3}/>
-      <Star starClick={handleStarClick} numberOfStars={2}/>
-      <Star starClick={handleStarClick} numberOfStars={1}/>
-    </div>
-  )
+    setStar(num);
+    setBrand("");
+    fetchProducts({ stars: num });
+  };
 
+  const showStars = () => (
+    <div className='pr-4 pl-4 pb-2'>
+      <Star starClick={handleStarClick} numberOfStars={5} />
+      <Star starClick={handleStarClick} numberOfStars={4} />
+      <Star starClick={handleStarClick} numberOfStars={3} />
+      <Star starClick={handleStarClick} numberOfStars={2} />
+      <Star starClick={handleStarClick} numberOfStars={1} />
+    </div>
+  );
+
+  // Show products based on brand
+  const showBrands = () =>
+    brands.map((b) => (
+      <div>
+        <Radio
+          value={b}
+          name={brand}
+          checked={b === brand}
+          onChange={handleBrand}
+          className='pb-1 pl-4 pr-4'>
+          {b}
+        </Radio>
+      </div>
+    ));
+
+  const handleBrand = (e) => {
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: { text: "" },
+    });
+    setPrice([0, 0]);
+    setCategoryIds([]);
+    setStar("");
+    setBrand(e.target.value);
+    fetchProducts({ brand: e.target.value });
+  };
 
   return (
     <div className='container-fluid'>
       <div className='row'>
         <div className='col-md-3 p-2'>
           <h4>Search/Filter</h4>
-          <Menu mode='inline' defaultOpenKeys={["1", "2", "3"]}>
+          <Menu mode='inline' defaultOpenKeys={["1", "2", "3", "4"]}>
             {/* Price */}
             <SubMenu
               key='1'
@@ -175,6 +217,7 @@ export const Shop = () => {
               }>
               <div style={{ marginTop: "-10px" }}>{showCategories()}</div>
             </SubMenu>
+
             {/* Stars */}
 
             <SubMenu
@@ -188,6 +231,17 @@ export const Shop = () => {
               <div style={{ marginTop: "-10px" }}>{showStars()}</div>
             </SubMenu>
 
+            {/* Brands */}
+            <SubMenu
+              key='4'
+              title={
+                <span className='h6'>
+                  <StarOutlined />
+                  Brands
+                </span>
+              }>
+              <div style={{ marginTop: "-10px" }}>{showBrands()}</div>
+            </SubMenu>
           </Menu>
         </div>
         <div className='col-md-9 p-2'>
