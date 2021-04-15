829c3bc126801c4afd16c89aa247140edf7ae808
Antony Parker <antonyqparker@gmail.com>
2021-04-15 17:56:14 +0200

    Saving products to cart in local storage

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -4,11 +4,33 @@ import { Card } from "antd";
 import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import laptop from "../../images/default.png";
 import { showAverage } from "../../actions/rating";
+import _ from 'lodash'
 
 const { Meta } = Card;
 
 export const ProductCard = ({ product }) => {
+
   const { description, title, images, slug, price } = product;
+
+  const handleAddToCart = () => {
+    // Create cart array
+    let cart  = []
+    if(typeof window !== 'undefined'){
+      // If cart is in localstorage, retrieve it
+      if(localStorage.getItem('cart')){
+        cart = JSON.parse(localStorage.getItem('cart'))
+      }
+      // Push new product into cart
+      cart.push({
+        ...product,
+        count: 1,
+      });
+      // Remove duplicates
+      let unique = _.uniqWith(cart, _.isEqual)
+      localStorage.setItem('cart', JSON.stringify(unique))
+    }
+  }
+
   return (
     <>
       {product && product.ratings && product.ratings.length > 0 ? (
@@ -32,11 +54,12 @@ export const ProductCard = ({ product }) => {
               View Product
             </>
           </Link>,
-          <>
+
+          <a onClick={handleAddToCart}>
             <ShoppingCartOutlined className='text-danger' />
             <br />
             Add To Cart
-          </>,
+          </a>,
         ]}>
         <Meta
           title={`${title} - $${price}`}
