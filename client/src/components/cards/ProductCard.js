32928808a7ebe2873c89aad62e2449cae7f35609
Antony Parker <antonyqparker@gmail.com>
2021-04-16 17:17:05 +0200

    Adding tooltip

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,24 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
-import { Card } from "antd";
+import { Card, Tooltip } from "antd";
 import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import laptop from "../../images/default.png";
 import { showAverage } from "../../actions/rating";
-import _ from 'lodash'
+import _ from "lodash";
 
 const { Meta } = Card;
 
 export const ProductCard = ({ product }) => {
-
   const { description, title, images, slug, price } = product;
+
+  const [tootTip, setToolTip] = useState("Clickto add");
 
   const handleAddToCart = () => {
     // Create cart array
-    let cart  = []
-    if(typeof window !== 'undefined'){
+    let cart = [];
+    if (typeof window !== "undefined") {
       // If cart is in localstorage, retrieve it
-      if(localStorage.getItem('cart')){
-        cart = JSON.parse(localStorage.getItem('cart'))
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
       }
       // Push new product into cart
       cart.push({
@@ -26,10 +27,12 @@ export const ProductCard = ({ product }) => {
         count: 1,
       });
       // Remove duplicates
-      let unique = _.uniqWith(cart, _.isEqual)
-      localStorage.setItem('cart', JSON.stringify(unique))
+      let unique = _.uniqWith(cart, _.isEqual);
+      localStorage.setItem("cart", JSON.stringify(unique));
+      // Show tool tip
+      setToolTip("Added");
     }
-  }
+  };
 
   return (
     <>
@@ -54,12 +57,13 @@ export const ProductCard = ({ product }) => {
               View Product
             </>
           </Link>,
-
-          <a onClick={handleAddToCart}>
-            <ShoppingCartOutlined className='text-danger' />
-            <br />
-            Add To Cart
-          </a>,
+          <Tooltip title={toolTip}>
+            <a onClick={handleAddToCart}>
+              <ShoppingCartOutlined className='text-danger' />
+              <br />
+              Add To Cart
+            </a>
+          </Tooltip>,
         ]}>
         <Meta
           title={`${title} - $${price}`}
