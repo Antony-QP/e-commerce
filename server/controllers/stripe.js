78eebd368240166393f818c95e29e194fadafb91
Antony Parker <antonyqparker@gmail.com>
2021-05-13 16:45:09 +0200

    Displaying stripe cart on front

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -2,18 +2,17 @@ const User = require('../models/user')
 const Cart = require('../models/cart')
 const Product = require('../models/product')
 const Coupon = require('../models/coupon')
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET}`);
 
 exports.createPaymentIntent = async (req, res) => {
     // Later apply coupon
     // Later calculate price
     const paymentIntent = await stripe.paymentIntents.create({
         amount: 100,
-        current: 'usd',
+        currency: 'usd',
     })
 
     res.send({
         clientSecret: paymentIntent.client_secret,
-        
     })
 }
