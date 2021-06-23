e1dc48e434db24f76c513cfda7b0606e9f17d20e
Antony Parker <antonyqparker@gmail.com>
2021-06-23 13:31:18 +0200

    Logging user orders on the front end

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import UserNav from '../../components/nav/UserNav'
+import { getUserOrders } from '../../actions/user'
+import { useSelector, useDispatch } from 'react-redux'
+import { CheckCircleOutlined, CloseCircleOutlined} from '@ant-design/icons'
+import { toast } from 'react-toastify'
 
 export const History = () => {
+
+    const [orders, setOrders] = useState([])
+    const { user } = useSelector((state) => ({ ...state }))
+
+    useEffect(() => {
+        loadUserOrders()
+    }, [])
+
+    const loadUserOrders = () => {
+        getUserOrders(user.token).then(res => {
+            console.log(JSON.stringify(res.data, null, 4))
+        })
+    }
+
     return (
         <div className="container-fluid">
             <div className="row">
