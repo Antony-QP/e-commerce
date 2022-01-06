5d2de4c12731abe0a30bf9b1c508c946732064c9
Antony Parker <antonyqparker@gmail.com>
2022-01-06 15:43:27 +0000

    Viewing orders in JSON in Dashboard

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,9 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNav from "../../components/nav/AdminNav";
+import { getOrders, changeStatus } from "../../actions/admin"
+import { useSelector, useDispatch } from 'react-redux'
+import { toast } from "react-toastify"
 
 
 export const AdminDashboard = () => {
   const [loading, setLoading] = useState(false);
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }))
+
+  useEffect(() => {
+    loadOrders()
+  }, [])
+
+
+  const loadOrders = () => {
+    getOrders(user.token).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setOrders(res.data);
+    })
+  }
+
+  const handleStatusChange = (orderId, orderStatus) => {
+    changeStatus(orderId, orderStatus, user.token).then(res => {
+      toast.success('Status Updated')
+      loadOrders();
+    })
+  }
 
   return (
     <div className='container-fluid'>
@@ -12,6 +36,7 @@ export const AdminDashboard = () => {
           <AdminNav />
         </div>
         <h4>Admin Dashboard</h4>
+        {JSON.stringify(orders)}
       </div>
     </div>
   );
