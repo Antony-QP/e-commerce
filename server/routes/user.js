3c4100a8597be0d8900cb64b6852487e39fe9239
Antony Parker <antonyqparker@gmail.com>
2022-01-07 15:42:12 +0000

    Wishlist routes and controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,7 @@ const {authCheck} = require('../middlewares/auth')
 const router = express.Router();
 
 // Controllers
-const { userCart, getUserCart, emptyCart, saveAddress, applyCouponToUserCart, createOrder, getOrders } = require('../controllers/user')
+const { userCart, getUserCart, emptyCart, saveAddress, applyCouponToUserCart, createOrder, getOrders, addToWishlist, wishlist, removeFromWishlist} = require('../controllers/user')
 
 router.post('/user/cart', authCheck, userCart)
 router.get('/user/cart', authCheck, getUserCart)
@@ -14,5 +14,11 @@ router.get('/user/orders', authCheck, getOrders)
 
 // Coupon
 router.post('/user/cart/coupon', authCheck, applyCouponToUserCart)
+
+// Wishlist
+
+router.post('user/wishlist', authCheck, addToWishlist)
+router.get('user/wishlist', authCheck, wishlist)
+router.put('user/wishlist/:productId', authCheck, removeFromWishlist)
 
 module.exports = router
